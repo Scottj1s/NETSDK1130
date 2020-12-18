3ae76e5c64329d582f417fdb1a51da493f25be97
Scott Jones <sjones@microsoft.com>
2020-12-18 09:49:51 -0800

    Add project files.

---
+++ NETSDK1130/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NETSDK1130
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
